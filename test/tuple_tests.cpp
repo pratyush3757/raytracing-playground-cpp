1a61fb16b2ddbd399aca884c216cd0a971ebf137
Pro3757 <pratyush3757@gmail.com>
2023-05-30 17:44:26 +0530

    formatting

--- test/tuple_tests.cpp
+++ test/tuple_tests.cpp
@@ -1,18 +1,7 @@
 #include "gtest/gtest.h"
 
-TEST(tupleTests, tupleTestCase1) {
-    
-    EXPECT_EQ(1, 1);
-}
+TEST(tupleTests, tupleTestCase1) { EXPECT_EQ(1, 1); }
 
-TEST(tupleTests, tupleTestCase2) {
-    
-    EXPECT_EQ(1, 1);
+TEST(tupleTests, tupleTestCase2) { EXPECT_EQ(1, 1); }
 
-}
-
-TEST(tupleTests, tupleTestCase3) {
-
-    EXPECT_EQ(1, 1);
-
-}
+TEST(tupleTests, tupleTestCase3) { EXPECT_EQ(1, 1); }
