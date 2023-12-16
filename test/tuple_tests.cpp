f1b359a7c8de1e9536803a3e234f6f349ab4bfee
Pro3757 <pratyush3757@gmail.com>
2023-12-16 19:38:44 +0530

    add rt_color and color tests

--- test/tuple_tests.cpp
+++ test/tuple_tests.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include <cmath>
 #include "../src/structs/rt_tuples.hpp"
+#include "../src/structs/rt_colors.hpp"
 
 TEST(tupleTests, check_is_point_is_vector) {
     rt_tuple point = rt_tuple(4.3l, -4.2l, 3.1l, 1.0l);
@@ -210,4 +211,54 @@ TEST(tupleTests, cross_product_of_two_vectors) {
 
     rt_tuple ref_2 = rt_tuple::vector(1, -2, 1);
     EXPECT_EQ(cross_product_2_1 == ref_2, true);
+}
+
+TEST(colorTests, colors_are_rgb_tuples) {
+    rt_color color_1 = rt_color(-0.5, 0.4, 1.7);
+    EXPECT_EQ(color_1.r(), -0.5);
+    EXPECT_EQ(color_1.g(), 0.4);
+    EXPECT_EQ(color_1.b(), 1.7);
+}
+
+TEST(colorTests, adding_colors) {
+    rt_color color_1 = rt_color(0.9, 0.6, 0.75);
+    rt_color color_2 = rt_color(0.7, 0.1, 0.25);
+    
+    rt_color added = color_1 + color_2;
+
+    rt_color ref = rt_color(1.6, 0.7, 1.0);
+
+    EXPECT_EQ(ref == added, true);
+}
+
+TEST(colorTests, subtracting_colors) {
+    rt_color color_1 = rt_color(0.9, 0.6, 0.75);
+    rt_color color_2 = rt_color(0.7, 0.1, 0.25);
+    
+    rt_color subtracted = color_1 - color_2;
+
+    rt_color ref = rt_color(0.2, 0.5, 0.5);
+
+    EXPECT_EQ(ref == subtracted, true);
+}
+
+TEST(colorTests, multiplying_color_by_scalar) {
+    rt_color color_1 = rt_color(0.2, 0.3, 0.4);
+    
+    rt_color multiplied = color_1 * 2;
+
+    rt_color ref = rt_color(0.4, 0.6, 0.8);
+
+    EXPECT_EQ(ref == multiplied, true);
+}
+
+TEST(colorTests, multiplying_colors) {
+    rt_color color_1 = rt_color(1, 0.2, 0.4);
+    rt_color color_2 = rt_color(0.9, 1, 0.1);
+    
+    rt_color multiplied = color_1 * color_2;
+
+    rt_color ref = rt_color(0.9, 0.2, 0.04);
+
+    EXPECT_EQ(ref == multiplied, true);
 }
